698b841b3105ab2ee50d8da973642bf048ba982a
Ahmad Raihan <ahmad.reyhan2230@gmail.com>
2025-02-19 17:55:15 +0700

    add id to todo

--- src/store/toDoStore.js
+++ src/store/toDoStore.js
@@ -1,22 +1,24 @@
-
-import { create } from 'zustand';
+import { create } from "zustand";
 
 const toDoStore = create((set) => ({
-    todos: JSON.parse(localStorage.getItem('todos')) || [],
-    addTodo: (todo) =>
-      set((state) => {
-        const updatedTodos = [...state.todos, todo];
-        localStorage.setItem('todos', JSON.stringify(updatedTodos));
-        return { todos: updatedTodos };
-      }),
+  todos: JSON.parse(localStorage.getItem("todos")) || [],
+  addTodo: (todo) =>
+    set((state) => {
+      const newTodo = { id: Date.now(), ...todo }; // Tambah ID unik
+      const updatedTodos = [...state.todos, newTodo];
+      localStorage.setItem("todos", JSON.stringify(updatedTodos));
+      return { todos: updatedTodos };
+      // const updatedTodos = [...state.todos, todo, ];
+      // localStorage.setItem('todos', JSON.stringify(updatedTodos));
+      // return { todos: updatedTodos };
+    }),
 
+  deleteTodo: (id) =>
+    set((state) => {
+      const updatedTodos = state.todos.filter((todo) => todo.id !== id);
+      localStorage.setItem("todos", JSON.stringify(updatedTodos));
+      return { todos: updatedTodos };
+    }),
+}));
 
-    deleteTodo: (id) =>
-      set((state) => {
-        const updatedTodos = state.todos.filter((todo) => todo.id !== id);
-        localStorage.setItem('todos', JSON.stringify(updatedTodos));
-        return { todos: updatedTodos };
-      }),
-  }));
-
-export default toDoStore
+export default toDoStore;
